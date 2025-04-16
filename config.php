873f996259e2bc834e912cc8389230e95cf12396
treybowman <123884817+treybowman@users.noreply.github.com>
2025-04-15 22:36:02 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'your_database',
+    'db_user' => 'your_username',
+    'db_pass' => 'your_password',
+    'log_token' => 'super-secret-token' // used in log_viewer.php
+];
