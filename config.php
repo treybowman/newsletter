8f7145e394e5e20edc7e0433e11f4c5329139f3b
treybowman <123884817+treybowman@users.noreply.github.com>
2025-04-16 22:11:18 -0400

    Update config.php

--- config.php
+++ config.php
@@ -5,9 +5,9 @@ return [
     'db_name'   => 'your_database_name',   // your Flarum/Newsletter DB name
     'db_user'   => 'your_database_user',   // your MySQL username
     'db_pass'   => 'your_database_pass',   // your MySQL password
-    'log_token' => 'super-secret-token'    // change to a strong, private value
-    'site_name' => 'MyCoolForum',
-    'site_url' => 'https://mycoolforum.com',
-    'users_table' => 'flarum_users',
-    'settings_table' => 'flarum_settings',
+    'log_token' => 'super-secret-token',    // change to a strong, private value
+    'site_name' => 'MyCoolForum',            // Flarum Site Name to apear on newsletter and pages
+    'site_url' => 'https://mycoolforum.com', // Flarum Domain for links
+    'users_table' => 'flarum_users',        // User table PREFIX_users
+    'settings_table' => 'flarum_settings'    // Settings table PREFIX_settings
 ];
