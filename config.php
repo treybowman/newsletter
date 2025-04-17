8ebb27209471f2897761240ea3a46a8d5cc63888
treybowman <123884817+treybowman@users.noreply.github.com>
2025-04-16 22:09:52 -0400

    Update config.php
    Adding site name, url, tables

--- config.php
+++ config.php
@@ -5,7 +5,9 @@ return [
     'db_name'   => 'your_database_name',   // your Flarum/Newsletter DB name
     'db_user'   => 'your_database_user',   // your MySQL username
     'db_pass'   => 'your_database_pass',   // your MySQL password
-
-    // Token to protect access to log_viewer.php
     'log_token' => 'super-secret-token'    // change to a strong, private value
+    'site_name' => 'MyCoolForum',
+    'site_url' => 'https://mycoolforum.com',
+    'users_table' => 'flarum_users',
+    'settings_table' => 'flarum_settings',
 ];
